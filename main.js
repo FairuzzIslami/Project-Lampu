5d56bee463387c4489f1a3c5a2ef2f2b9e1b8338
Fairuz <firdauz2007123@gmail.com>
2024-05-07 21:47:23 +0700

    project done

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-function btn(params){ //  ==> mengambil nilai dari function yang di buat ,seperti variabel nya params
-    console.log(params)  // dan nilai/valuenya itu dari 'on'
-
+function btn(action,numbLamp){ //  ==> mengambil nilai dari function yang di buat ,seperti variabel nya params
+    console.log(action)  // dan nilai/valuenya itu dari 'on'
     // console.log("hello world")
     // const lampuMati = document.getElementById('ofLampu')
     // console.log(lampuMati.src) //src berguana untuk mengatahui nama link image/ gambar kita,==> http://127.0.0.1:5500/Images/off.gif
@@ -8,10 +7,34 @@ function btn(params){ //  ==> mengambil nilai dari function yang di buat ,sepert
     const lampuOf = document.getElementById('ofLampu')
     const lampuOf2 = document.getElementById('ofLampu2')
     const lampuOf3 = document.getElementById('ofLampu3')
-    if(params == 'on'){
-        lampuOf.src = 'images/on.gif'
-    }else if(params == 'of'){
-        lampuOf.src = 'images/off.gif'
-    }
-    return lampuOf
+    switch(action){
+        case 'on':
+            switch(numbLamp){
+                case 1:
+                    lampuOf.src = 'asset/images/on.gif'
+                    break;
+                case 2:
+                    lampuOf2.src = 'asset/images/on.gif'
+                    break;
+                case 3:
+                    lampuOf3.src = 'asset/images/on.gif'
+                    break
+            }
+            break; // gunakan selalu break ketika ganti suatu kondisi
+
+        case 'of':
+            switch(numbLamp){
+                case 1:
+                    lampuOf.src = 'asset/images/off.gif'
+                    break;
+                case 2:
+                    lampuOf2.src = 'asset/images/off.gif'
+                    break;
+                case 3:
+                    lampuOf3.src='asset/images/off.gif'
+                    break;
+            }
+            break;
+        }
+    return lampuOf,numbLamp
 }
