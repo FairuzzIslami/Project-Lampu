bf8afaa81ac2b97febdf2b7a2476189c965ca64a
Fairuz <firdauz2007123@gmail.com>
2024-05-08 20:44:13 +0700

    chance btn to toggle btn

--- main.js
+++ main.js
@@ -1,40 +1,47 @@
-function btn(action,numbLamp){ //  ==> mengambil nilai dari function yang di buat ,seperti variabel nya params
-    console.log(action)  // dan nilai/valuenya itu dari 'on'
-    // console.log("hello world")
-    // const lampuMati = document.getElementById('ofLampu')
-    // console.log(lampuMati.src) //src berguana untuk mengatahui nama link image/ gambar kita,==> http://127.0.0.1:5500/Images/off.gif
-    // lampuMati.src = 'images/on.gif'
+function btn(){ 
+    let btnToggle = document.getElementById('btn-toggle')
+    let btnToggle2 = document.getElementById('btn-toggle2')
+    let btnToggle3 = document.getElementById('btn-toggle3')
     const lampuOf = document.getElementById('ofLampu')
     const lampuOf2 = document.getElementById('ofLampu2')
     const lampuOf3 = document.getElementById('ofLampu3')
-    switch(action){
-        case 'on':
-            switch(numbLamp){
-                case 1:
-                    lampuOf.src = 'asset/images/on.gif'
-                    break;
-                case 2:
-                    lampuOf2.src = 'asset/images/on.gif'
-                    break;
-                case 3:
-                    lampuOf3.src = 'asset/images/on.gif'
-                    break
-            }
-            break; // gunakan selalu break ketika ganti suatu kondisi
+    console.log(btnToggle.checked)
 
-        case 'of':
-            switch(numbLamp){
-                case 1:
-                    lampuOf.src = 'asset/images/off.gif'
-                    break;
-                case 2:
-                    lampuOf2.src = 'asset/images/off.gif'
-                    break;
-                case 3:
-                    lampuOf3.src='asset/images/off.gif'
-                    break;
-            }
-            break;
-        }
-    return lampuOf,numbLamp
-}
+    switch(btnToggle.checked){
+        case true:
+            lampuOf.src = 'asset/images/on.gif'
+        break
+        case false:
+            lampuOf.src = 'asset/images/off.gif'
+            break
+    }
+    
+    switch(btnToggle2.checked){
+        case true:
+            lampuOf2.src = 'asset/images/on.gif'
+        break
+        case false:
+            lampuOf2.src = 'asset/images/off.gif'
+            break
+    }
+    
+    switch(btnToggle3.checked){
+        case true:
+            lampuOf3.src = 'asset/images/on.gif'
+        break
+        case false:
+            lampuOf3.src = 'asset/images/off.gif'
+            break
+    } 
+
+    
+                }
+                
+                //function(params){//  ==> mengambil nilai dari function yang di buat ,seperti variabel nya params
+                    //console.log(action)  // dan nilai/valuenya itu dari 'on'
+                    // console.log("hello world")
+                    // const lampuMati = document.getElementById('ofLampu')
+                    // console.log(lampuMati.src) //src berguana untuk mengatahui nama link image/ gambar kita,==> http://127.0.0.1:5500/Images/off.gif
+                    // lampuMati.src = 'images/on.gif'
+                //}
+                    
